8243386ed7a65c53df0fc648c524ff7a0031e8c1
BantuD <500069424@stu.upes.ac.in>
2019-03-28 11:08:15 +0530

    committing after error

--- file.cpp
+++ file.cpp
@@ -8,18 +8,27 @@ public:
     cout<<"Enter two num to add: ";
     cin>>i>>k;
     cout<<endl;
-    cout<<"Addition of two numbs: "<<i+k;
+    cout<<"Addition of two numbs: "<<i+k<<endl;
     }
+    void sub(){
+    cout<<"Enter two numbs to sub: ";
+    cin>>i>>k;
+    cout<<endl;
+    cout<<"The subtraction two numbs: "<<i-k;
+    }
+    void mul(){
+    cout<<"Enter two numb to multiply ";
+    cin>>i>>k;
+    cout<<endl;
+    cout<<"The multiplication of two numbs: "<<i*k<<endl;
 
-   void sub(){
-  cout<<"Enter two numb to sub: ";
-  cin>>i>>k;
-
-
+    }
 };
 int main(){
 A a;
 a.add();
+a.sub();
+a.mul();
 return 0;
 }
 
